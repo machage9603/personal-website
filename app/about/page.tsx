f9e71242fbedee4156a482239e9fe4e6d5a7d3e6
machage9603 <mikemachage@gmail.com>
2024-09-16 14:53:43 +0300

    install missing depedancies

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
-import { Download } from 'lucide-react'
-import { motion } from 'framer-motion'
+import { Download } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
@@ -10,18 +10,23 @@ export default function About() {
         <motion.div
           className="w-full h-px bg-[#2B2B2B] dark:bg-white mb-8"
           initial={{ width: 0 }}
-          animate={{ width: '100%' }}
+          animate={{ width: "100%" }}
           transition={{ duration: 1 }}
         ></motion.div>
         <h1 className="text-3xl font-bold mb-6">About Me</h1>
         <p className="mb-4">
-          Hi, I am Mike Machage, a Software Engineer with a passion for building scalable and efficient web applications.
+          Hi, I am Mike Machage, a Software Engineer with a passion for building
+          scalable and efficient web applications.
         </p>
         <p className="mb-4">
-          I will help you think through your business idea and make it a reality in less than 90 days.My expertise lies in full-stack development, with a focus on JavaScript technologies like Next.js, React, Node.js, and TypeScript. I am always eager to learn and adapt to new technologies and methodologies.
+          I will help you think through your business idea and make it a reality
+          in less than 90 days.My expertise lies in full-stack development, with
+          a focus on JavaScript technologies like Next.js, React, Node.js, and
+          TypeScript. I am always eager to learn and adapt to new technologies
+          and methodologies.
         </p>
         <motion.a
-          href="/mikemachageresume.pdf"
+          href="/machageCV.pdf"
           download
           className="flex items-center justify-center px-6 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition-colors duration-300"
           whileHover={{ scale: 1.05 }}
@@ -33,10 +38,10 @@ export default function About() {
         <motion.div
           className="w-full h-px bg-[#2B2B2B] dark:bg-white mt-8"
           initial={{ width: 0 }}
-          animate={{ width: '100%' }}
+          animate={{ width: "100%" }}
           transition={{ duration: 1 }}
         ></motion.div>
       </div>
     </div>
-  )
+  );
 }
