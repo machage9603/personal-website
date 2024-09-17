937414eb97a1193d64a85b6b0016b4a46f797b36
machage9603 <mikemachage@gmail.com>
2024-09-17 12:56:20 +0300

    add google analytics script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Inter } from "next/font/google";
 import "./ui/globals.css";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +10,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-P78HP4L4W7";
+    document.body.appendChild(script);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-P78HP4L4W7');
+    `;
+    document.body.appendChild(script2);
+  }, []);
+
   return (
     <html lang="en">
       <body
