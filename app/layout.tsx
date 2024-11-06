53def08ca1772a9e458148e43f49276ceb62d990
machage9603 <mikemachage@gmail.com>
2024-11-06 19:58:36 +0300

    improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,11 @@
 
 import { Inter } from "next/font/google";
 import "./ui/globals.css";
-import { useEffect } from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,31 +15,35 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-P78HP4L4W7";
-    document.body.appendChild(script);
-
-    const script2 = document.createElement("script");
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-P78HP4L4W7');
-    `;
-    document.body.appendChild(script2);
-  }, []);
-
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
-      </head>
-      <body
-        className={`${inter.className} min-h-screen flex flex-col transition-colors duration-300`}
-      >
+      <Head>
+        <title>Mike Machage | Software Engineer</title>
+        <meta name="description" content="UI/UX Designer, Full Stack developer." />
+        <meta name="keywords" content="software engineer, web development, web design, UI/UX, Javascript, Next.js, TypeScript" />
+        <link rel="canonical" href="https://mikemachage.tech/" />
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
+
+        {/* Structured Data */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Mike Machage",
+            "url": "https://mikemachage.tech/",
+            "sameAs": [
+              "https://www.linkedin.com/in/mike-machage",
+              "https://x.com/SerMachage"
+            ],
+            "jobTitle": "Software Engineer",
+            "worksFor": {
+              "@type": "Organization",
+              "name": "Darasani Hub"
+            }
+          })}
+        </script>
+      </Head>
+      <body className={`${inter.className} min-h-screen flex flex-col transition-colors duration-300`}>
         <NextUIProvider>
           <NextThemesProvider
             attribute="class"
