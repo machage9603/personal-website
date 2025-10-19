c863286f7922606213c97e16a4267f3b8ab08fc1
machage9603 <mikemachage@gmail.com>
2025-10-19 21:16:00 +0300

    refactor(page): change width to full

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className={`min-h-screen bg-gray-950 text-gray-100 ${montserrat.className} font-mono flex flex-col`}>
+    <div className={`w-full min-h-screen bg-gray-950 text-gray-100 ${montserrat.className} font-mono flex flex-col`}>
       <div className="absolute inset-0 bg-[linear-gradient(rgba(255,255,255,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.02)_1px,transparent_1px)] bg-[size:64px_64px]"></div>
 
       <Navbar />
